119e45d2e863a95ed49339a0c2b0347124117a48
이태훈 <rhymint@gmail.com>
2024-02-18 23:06:43 +0900

    init for step2

--- src/App.js
+++
@@ -1,9 +0,0 @@
-import RacingCarGame from "./RacingCarGame/RacingCarGame.js";
-
-class App {
-  async play() {
-    const racingCarGame = new RacingCarGame();
-  }
-}
-
-export default App;
--- src/constants/RegExp.js
+++
@@ -1,5 +0,0 @@
-const REGEXP = Object.freeze({
-  carName: /^[a-z가-힣]+$/,
-});
-
-export default REGEXP;
